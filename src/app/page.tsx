8f31b03ebbf072a987024e2cf1fc8f07d360ce3e
KevinHuang <eevee.vt.pt@gmail.com>
2024-05-04 10:57:08 +0700

    fix: resolve lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
-import { SwapBox } from "@/components";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+import { SwapBox } from "@/components";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between overflow-x-hidden">
       <SwapBox />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          padding: 12,
-        }}
-      >
-        <div>
-          <ConnectButton />
-        </div>
-      </div>
+      <ConnectButton />
     </main>
   );
 }
